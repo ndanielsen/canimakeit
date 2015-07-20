2373ae7bdc5cf2477ba08cce5b8dfac41db2f0b5
Nathan Danielsen <nathanjdanielsen@gmail.com>
2015-07-19 21:49:48 -0400

    added template files in ingest and wrange

---
+++ wrangle/cl_extractor.py
@@ -0,0 +1,100 @@
+!#/usr/bin/env python
+# encoding: utf-8
+
+### FILE DOC-STRING ###########################################
+"""
+cl_extractor.py
+
+Extracts Data from Craigslist Apartment HTML File
+
+Can I Make It? DDL Project (C)2015
+"""
+
+### IMPORTS ###################################################
+### By standard libary (in alphabetical order )
+
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+from builtins import (
+         bytes, dict, int, list, object, range, str,
+         ascii, chr, hex, input, next, oct, open,
+         pow, round, super,
+         filter, map, zip)
+
+import this
+
+
+### Third party import (in alphabetical order )
+
+
+### Location application / library specific imports
+
+
+
+### CONSTANTS #################################################
+
+
+
+### EXCEPTION CLASSES #########################################
+
+
+
+### INTERFACE FUNCTIONS #######################################
+
+def function():
+    """
+    Inputs:
+
+    What function does....
+
+    Outputs:
+    """
+    pass   
+
+
+### CLASSSES #################################################
+
+class ClassName(object):
+    """
+    Inputs:
+
+    Functionalities: 
+
+    Inheritance: 
+    
+    Outputs:
+    """
+
+    def __init__(self, arg):
+        super(ClassName, self).__init__()
+        self.arg = arg
+        
+
+
+### MAIN #####################################################
+
+def main():
+
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+##############################################################
+#### TESTS
+from nose.tools import *
+import unittest
+
+
+class ClassName(unittest.TestCase):
+
+    def setUp(self):
+        self.arg = ClassName.arg
+
+    def test_the_world_is_sane(self):
+        self.assertEquals(1+1, 2)
+
