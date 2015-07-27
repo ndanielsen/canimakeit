197f4e3f9adf8aea822497d3f46b97de7a25d367
nathan <nathan.danielsen@gmail.com>
2015-07-27 00:28:58 +0000

    added modeling api with test data

---
+++ model/admin.py
@@ -0,0 +1,61 @@
+#! usr/bin/env python -tt
+"""Recommendation Engine for Can I Make It 
+
+Input: User supplied information such salary, household size and etc
+
+Output: Returns nested dictionary of 3 budget types (min, mean, max) with a housing recommendation for each
+
+By Nathan Danielsen
+DDL4 Team
+"""
+
+from model.predict import Budgeter, Lodger
+
+
+### TEST CASES 
+
+user1 = 35,000
+user2 = 70,000
+user3 = 100,000
+
+class Base(object):
+	pass
+
+class Recommend(Base):
+	""" 
+	Inherits from Base-Object
+
+	Returns a nested dictionary of budgets each with housing recommendation
+	"""
+	def __init__(self):
+		
+		self.budgeting = Budgeter()
+		self.lodging = Lodger() # returns three lodging options that fit 
+
+
+	def recommend(self, user_salary):
+		user_recommendations = {}
+
+		budgets = self.budgeting.predict(user_salary)
+
+		for b in budgets.keys():
+			housing = budgets[b]['housing']
+			lodging_options = self.lodging.predict(housing) # returns dictionary of recommendations
+			budgets[b].update({'lodging':lodging_options})
+			user_recommendations.update(budgets[b])
+
+		return user_recommendations
+
+		
+
+if __name__ == '__main__':
+	main = Recommend()
+	print main.recommend(10000)
+
+	lodge = Lodger()
+	assert lodge.predict(1000, preferences="this")[1]['type'] == '1BR'
+	print 'test lodger passed'
+
+	budget = Budgeter()
+	assert budget.predict(40000)['max']['housing'] == 6000.0
+	print 'test budget passed'
---
+++ model/model/predict.py
@@ -0,0 +1,84 @@
+#! usr/bin/env python -tt
+"""
+Prediction using trained models in a class format
+"""
+
+class ModelBase(object):
+	def __init__(self):
+		self.DIR = None
+		pass
+
+	def load_model(self, dir):
+		model_loaded_successfully = False
+		if model_loaded_successfully: ### To refactor after MVP
+			pass
+		else:
+			return None
+
+	def model_predict(self, x, z):
+		pass
+
+	def test(self, x):
+		''' assert test'''
+		assert 2 + 2  == 4
+		
+
+class Budgeter(ModelBase):
+	def __init__(self):
+		self.DIR = 'budget/'
+
+
+	def breakdown(self, user_salary): # 
+		if self.load_model(self.DIR):
+			return self.model_predict(user_salary)
+		else:
+			return self.mvp_breakdown(user_salary) 
+	
+	def mvp_breakdown(self, user_salary): ### develop catgories and budget precentages
+		self.test
+		
+		budgets = {'max' : {'housing':user_salary*.15, 'grocery':None},
+          'mean' : {'housing':user_salary*.33, 'grocery':None},
+          'min' : {'housing':user_salary*.50, 'grocery':None}}
+
+		return budgets
+
+	def predict(self, user_salary):
+		return self.breakdown(user_salary)
+
+
+
+
+
+class Lodger(ModelBase):
+
+	def __init__(self):
+		self.DIR = 'lodge/'
+
+	def housingchoices(self, housing, preferences=None):
+		if self.load_model(self.DIR) != None:
+			return self.model_predict(housing, preferences)
+		else:
+			return self.mvp_housing(housing, preferences)
+
+	def mvp_housing(self, housing, preferences):
+		
+		lodging_options = {
+		1: {'type':"1BR", "rent":1400, "neighborhood":"Columbia Heights", 'url':None},
+		2: {'type':"2BR", "rent":1000, "neighborhood":"Anacostia", 'url':None},
+		3: {'type':"Studio", "rent":1900, "neighborhood":"Georgetown", 'url':None},
+		}
+
+		return lodging_options
+
+	def predict(self, housing, preferences=None):
+		return self.housingchoices(housing, preferences)
+
+if __name__ == '__main__':
+	lodge = Lodger()
+	assert lodge.predict(1000, preferences="this")[1]['type'] == '1BR'
+	print 'test lodger passed'
+
+	budget = Budgeter()
+	assert budget.predict(40000)['max']['housing'] == 6000.0
+	print 'test budget passed'
